98cfff75c5dc25e40d51709331907fe38bee1f97
willysafari <willymkumbwa@gmail.com>
2026-02-19 15:49:22 +0300

    caegory api updates

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+
+
 
 class AuthController extends Controller
 {
@@ -18,9 +24,52 @@ class AuthController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function register(Request $request)
     {
         //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:300',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+            'password_confirmation' => 'required|min:8',
+            'role' => 'nullable|in:admin,author,reader',
+             'profile_picture' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048|dimensions:min_width=100,min_height=100'
+        ]);
+
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'failed',
+                'error' => $validator->errors()
+            ], 401);
+        }
+
+        $user = $request->all();
+
+    // image uploaded
+
+    $imagePath=null;
+
+    if($request->hasFile('profile_picture') && $request->file('profile_picture')->isvalid()){
+        $file = $request->file('profile_picture');
+        $fileName= time().'_'.$file->getClientOriginalName();
+
+        // move the file to public directory
+        $file ->move(public_path('storage/profile'), $fileName);
+
+        // save the relative path to database
+        $imagePath = "storage/profile/".$fileName;
+    }
+
+    $user['profile_picture'] = $imagePath;
+
+        User::create($user);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'data created successfully'
+        ], 201);
+
+
     }
 
     /**
@@ -45,5 +94,62 @@ class AuthController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    public function login(Request $request)
+    {
+        // validate data
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fails',
+                'message' => $validator->errors()
+            ], 401);
+        }
+
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $datas['token'] = $user->createToken('SchoolApp')->plainTextToken;
+            $datas['name'] = $user->name;
+            $datas['email'] = $user->email;
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Login successfully',
+                'data' => $datas,
+            ], 201);
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                ' error' => 'email or password not corrent'
+            ], 400);
+        }
+    }
+
+    public function profile()
+    {
+
+        $user = Auth::user();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ], 201);
+    }
+    public function logout()
+    {
+
+        $user = Auth::user();
+        $user->tokens()->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'logout successfully '
+        ]);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+route::post('/register',[AuthController::class,'register'])->name('register');
+route::post('/login',[AuthController::class,'login'])->name('login');
+
+route::group(['middleware'=>'auth:sanctum'], function(){
+   route::get('/profile',[AuthController::class,'profile'])->name('profile');
+   route::get('/logout',[AuthController::class,'logout'])->name('logout');
+});
+
