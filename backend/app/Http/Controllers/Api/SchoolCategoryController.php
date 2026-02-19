4dca4cdf6d98843e892625440ca1d3e9ef5dd08a
willysafari <willymkumbwa@gmail.com>
2026-02-19 17:26:46 +0300

    post apis

--- backend/app/Http/Controllers/Api/SchoolCategoryController.php
+++ backend/app/Http/Controllers/Api/SchoolCategoryController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\SchoolCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class SchoolCategoryController extends Controller
 {
@@ -12,7 +15,14 @@ class SchoolCategoryController extends Controller
      */
     public function index()
     {
-        //
+        // to get all
+
+        $category = SchoolCategory::all();
+
+        return response()->json([
+            'status' => 'suceess',
+            'data' => $category,
+        ]);
     }
 
     /**
@@ -20,7 +30,34 @@ class SchoolCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // add category
+
+        $validatorData = Validator::make($request->all(), [
+            'name' => 'required|String|max:255',
+
+        ]);
+
+        if ($validatorData->fails()) {
+            return response()->json([
+                'status' => 'success',
+                'data' => $validatorData->errors(),
+            ]);
+        }
+
+        $data['name'] = $request->name;
+        $data['slug'] = Str::slug($request->name);
+
+
+        SchoolCategory::create($data);
+
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'category created successfully',
+            'data' => $data,
+        ]);
+
+
     }
 
     /**
@@ -37,13 +74,56 @@ class SchoolCategoryController extends Controller
     public function update(Request $request, string $id)
     {
         //
-    }
+        $categoryid = SchoolCategory::find($id);
 
+        $validatorData = Validator::make($request->all(), [
+            'name' => 'required|String|max:255',
+
+        ]);
+
+        if ($validatorData->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'data' => $validatorData->errors(),
+            ]);
+        }
+        if ($categoryid) {
+            $data['name'] = $request->name;
+            $data['slug'] = Str::slug($request->name);
+            SchoolCategory::create($data);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'category updated successfully',
+                'data' => $data,
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'the category not found',
+            ]);
+        }
+    }
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
         //
+        $category = SchoolCategory::find($id);
+
+        if ($category) {
+            SchoolCategory::destroy($id);
+              return response()->json([
+                'status' => 'success',
+                'message' => 'category deleted successfully',
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'fail',
+                'message' => 'category id not found',
+            ],400);
+        }
+
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\SchoolCategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,8 @@ route::post('/login',[AuthController::class,'login'])->name('login');
 route::group(['middleware'=>'auth:sanctum'], function(){
    route::get('/profile',[AuthController::class,'profile'])->name('profile');
    route::get('/logout',[AuthController::class,'logout'])->name('logout');
+
+   route::apiResource('categories', SchoolCategoryController::class);
+
 });
 
